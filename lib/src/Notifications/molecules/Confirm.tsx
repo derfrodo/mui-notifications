87a23234861442e1e46f557e3980b46534cfb12a
Stefan Pauka <derfrodo0715@gmail.com>
2020-10-31 22:28:29 +0100

    icons

---
+++ lib/src/Notifications/molecules/Confirm.tsx
@@ -0,0 +1,225 @@
+import {
+    Box,
+    Button,
+    createStyles,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Grid,
+    IconButton,
+    makeStyles,
+    Typography,
+    useTheme,
+} from "@material-ui/core";
+import { Clear, Error, Help, Info, Warning } from "@material-ui/icons";
+import React, { useMemo } from "react";
+import ConfirmData, { ConfirmTypes } from "./../interfaces/ConfirmData";
+
+const useConfirmStyles = makeStyles((t) =>
+    createStyles({
+        dialogRoot: {
+            minWidth: 300,
+            width: "80%",
+            // maxWidth: 600,
+            [t.breakpoints.down("xs")]: {
+                width: 300,
+            },
+        },
+        titleRoot: {
+            height: 56,
+        },
+        iconBox: {
+            height: 56,
+        },
+        titleBox: {
+            height: 56,
+        },
+        title: {
+            [t.breakpoints.down("sm")]: {
+                fontSize: "1.1rem",
+            },
+        },
+        messagePre: {
+            whiteSpace: "pre-wrap",
+        },
+    })
+);
+
+export const Confirm: React.FC<{ confirmData?: ConfirmData }> = ({
+    confirmData,
+}) => {
+    const classes = useConfirmStyles();
+    const { message, title, onClose, onConfirm, onDeny, type } =
+        confirmData || {};
+
+    const titleWidth = useMemo(
+        () =>
+            !onConfirm && !onDeny && type
+                ? 8
+                : type || onConfirm || onDeny
+                ? 9
+                : 12,
+        [onConfirm, onDeny, type]
+    );
+
+    const titleWidthSm = useMemo(
+        () =>
+            !onConfirm && !onDeny && type
+                ? 8
+                : type || onConfirm || onDeny
+                ? 10
+                : 12,
+        [onConfirm, onDeny, type]
+    );
+
+    const theme = useTheme();
+    const darkMode = !!theme;
+    const typeIcon = useMemo(() => {
+        switch (type) {
+            case ConfirmTypes.ERROR:
+                return <Error htmlColor={theme.palette.error.main} />;
+            case ConfirmTypes.INFO:
+                return (
+                    <Info
+                        htmlColor={
+                            darkMode
+                                ? theme.palette.getContrastText(
+                                      theme.palette.background.default
+                                  )
+                                : theme.palette.primary.main
+                        }
+                    />
+                );
+            case ConfirmTypes.WARN:
+                return <Warning htmlColor={theme.palette.warning.main} />;
+            case ConfirmTypes.QUESTION:
+                return (
+                    <Help
+                        htmlColor={
+                            darkMode
+                                ? theme.palette.getContrastText(
+                                      theme.palette.background.default
+                                  )
+                                : theme.palette.primary.main
+                        }
+                    />
+                );
+            default:
+                return <></>;
+        }
+    }, [darkMode, theme.palette, type]);
+
+    return (
+        <Dialog
+            open={!!confirmData && !confirmData?.closed}
+            maxWidth={confirmData?.maxWidth ?? "sm"}
+            classes={{
+                paper: classes.dialogRoot,
+            }}
+        >
+            <DialogTitle>
+                <Grid
+                    container
+                    spacing={2}
+                    classes={{ root: classes.titleRoot }}
+                >
+                    {type ? (
+                        <Grid item xs={2} sm={2}>
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="flex-start"
+                                className={classes.iconBox}
+                            >
+                                {typeIcon}
+                            </Box>
+                        </Grid>
+                    ) : (
+                        ""
+                    )}
+                    <Grid item xs={titleWidth} sm={titleWidthSm}>
+                        <Box
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="flex-start"
+                            className={classes.titleBox}
+                        >
+                            {typeof title === "string" ? (
+                                <Typography
+                                    variant="h3"
+                                    component="h2"
+                                    className={classes.title}
+                                >
+                                    {title}
+                                </Typography>
+                            ) : title ? (
+                                title
+                            ) : (
+                                "Confirm"
+                            )}
+                        </Box>
+                    </Grid>
+                    {!onConfirm && !onDeny ? (
+                        <Grid item xs={2} sm={2}>
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="flex-end"
+                                className={classes.iconBox}
+                            >
+                                <IconButton onClick={onClose}>
+                                    <Clear />
+                                </IconButton>
+                            </Box>
+                        </Grid>
+                    ) : (
+                        ""
+                    )}
+                </Grid>
+            </DialogTitle>
+            <DialogContent>
+                {typeof message === "string" ? (
+                    <Typography
+                        variant="body1"
+                        component="pre"
+                        className={classes.messagePre}
+                    >
+                        {message}
+                    </Typography>
+                ) : (
+                    message
+                )}
+            </DialogContent>
+            <DialogActions>
+                {onConfirm || onDeny ? (
+                    <Button
+                        onClick={onDeny || onClose}
+                        color="secondary"
+                        variant={darkMode ? "contained" : "text"}
+                    >
+                        Nein
+                    </Button>
+                ) : (
+                    ""
+                )}
+                {onConfirm || onDeny ? (
+                    <Button
+                        onClick={onConfirm || onClose}
+                        color="primary"
+                        variant={darkMode ? "contained" : "text"}
+                    >
+                        Ja
+                    </Button>
+                ) : (
+                    ""
+                )}
+                {!onConfirm && !onDeny ? (
+                    <Button onClick={onClose}>Ok</Button>
+                ) : (
+                    ""
+                )}
+            </DialogActions>
+        </Dialog>
+    );
+};
--- lib/src/Snackbar/useAddSnackbar.ts
+++ lib/src/Snackbar/useAddSnackbar.ts
@@ -1,59 +1,59 @@
-import {
-  useAppReducerContextDispatch,
-  appActionCreators,
-} from "../App/reducer";
-import { SnackbarData, SNACKBAR_TYPES } from "../App/interfaces/SnackbarData";
 import { useCallback, useMemo } from "react";
+import { SnackbarData, SNACKBAR_TYPES } from "./interfaces/SnackbarData";
+import {
+    snackbarActionCreators,
+    useSnackbarReducerContextDispatch,
+} from "./reducer";
 
 export const useAddSnackbar = () => {
-  const dispatch = useAppReducerContextDispatch();
+    const dispatch = useSnackbarReducerContextDispatch();
 
-  const addSnackbar = useCallback(
-    (next: SnackbarData & { closed?: false | undefined }) => {
-      dispatch(appActionCreators.addSnackbarData(next));
-    },
-    [dispatch]
-  );
+    const addSnackbar = useCallback(
+        (next: SnackbarData & { closed?: false | undefined }) => {
+            dispatch(snackbarActionCreators.addSnackbarData(next));
+        },
+        [dispatch]
+    );
 
-  const add = useCallback(
-    (message: string, title?: string, type?: SNACKBAR_TYPES) => {
-      addSnackbar({
-        message,
-        type,
-        title,
-      });
-    },
-    [addSnackbar]
-  );
+    const add = useCallback(
+        (message: string, title?: string, type?: SNACKBAR_TYPES) => {
+            addSnackbar({
+                message,
+                type,
+                title,
+            });
+        },
+        [addSnackbar]
+    );
 
-  const addSuccess = useCallback(
-    (message: string, title?: string) => {
-      add(message, title, SNACKBAR_TYPES.SUCCESS);
-    },
-    [add]
-  );
+    const addSuccess = useCallback(
+        (message: string, title?: string) => {
+            add(message, title, SNACKBAR_TYPES.SUCCESS);
+        },
+        [add]
+    );
 
-  const addError = useCallback(
-    (message: string, title?: string) => {
-      add(message, title, SNACKBAR_TYPES.ERROR);
-    },
-    [add]
-  );
-  const addWarning = useCallback(
-    (message: string, title?: string) => {
-      add(message, title, SNACKBAR_TYPES.WARNING);
-    },
-    [add]
-  );
+    const addError = useCallback(
+        (message: string, title?: string) => {
+            add(message, title, SNACKBAR_TYPES.ERROR);
+        },
+        [add]
+    );
+    const addWarning = useCallback(
+        (message: string, title?: string) => {
+            add(message, title, SNACKBAR_TYPES.WARNING);
+        },
+        [add]
+    );
 
-  return useMemo(
-    () => ({
-      add,
-      addError,
-      addSuccess,
-      addSnackbar,
-      addWarning,
-    }),
-    [add, addError, addSnackbar, addSuccess, addWarning]
-  );
+    return useMemo(
+        () => ({
+            add,
+            addError,
+            addSuccess,
+            addSnackbar,
+            addWarning,
+        }),
+        [add, addError, addSnackbar, addSuccess, addWarning]
+    );
 };

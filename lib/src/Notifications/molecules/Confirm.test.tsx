a038b33441a7cb64f771f084daf082ee708b8678
Stefan Pauka <derfrodo0715@gmail.com>
2020-11-01 14:15:22 +0100

    add test file

---
+++ lib/src/Notifications/molecules/Confirm.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import { Confirm } from "./Confirm";
+
+describe("Confirm Component Tests", () => {
+    it("renders component", async () => {
+        const { getByTestId } = render(<Confirm />);
+        await waitFor(
+            () => {
+                const hiMessage = getByTestId("confirmDialog");
+                expect(hiMessage).toBeInTheDocument();
+            },
+            { timeout: 200 }
+        );
+    });
+});
